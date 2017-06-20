8f0732bbb684f1ccecab20ccc6802d753d570615
Sagar Jha <sagar.jhaa@gmail.com>
2017-06-20 15:37:50 -0400

    Change member table

---
+++ Template.Models/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Models
+{
+    public interface IAuditable
+    {
+        DateTimeOffset DateCreated { get; set; }
+
+        DateTimeOffset? DateEdited { get; set; }
+
+        string MemberCreatedId { get; set; }
+
+        string MemberEditedId { get; set; }
+    }
+}
--- Template.Models/Member.cs
+++ Template.Models/Member.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Template.Models
 {
-    public class Member : IModel, IOrganizationModel, IMemberBase
+    public class Member : IModel, IOrganizationModel, IMemberBase , IAuditable
     {
         public string Id { get; set; }
         [Required]
@@ -26,5 +28,50 @@ namespace Template.Models
         {
             return string.Format("{1}, {0}", FirstName, LastName);
         }
+
+        [DisplayName("Home Phone")]
+        public string Phone { get; set; }
+
+
+        public string Extension { get; set; }
+
+        [DisplayName("Cell Phone")]
+        public string MobilePhone { get; set; }
+
+        public string Website { get; set; }
+
+        public string HouseNumber { get; set; }
+
+
+        public string StreetPrefix { get; set; }
+
+
+        public string StreetName { get; set; }
+
+
+        public string StreetSuffix { get; set; }
+
+
+        public string Address2 { get; set; }
+
+
+        public string CityName { get; set; }
+
+        [DisplayName("State")]
+        public string StateAbbreviation { get; set; }
+
+        [DisplayName("Zip Code")]
+        public string Zip5 { get; set; }
+
+
+        public string Zip4 { get; set; }
+
+        public byte Active { get; set; }
+
+
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset? DateEdited { get; set; }
+        public string MemberCreatedId { get; set; }
+        public string MemberEditedId { get; set; }
     }
 }

f309a7604be89da59a0bfd1f67b0608375bd1231
Sagar Jha <sagar.jhaa@gmail.com>
2017-06-20 15:34:33 -0400

    second commit

--- Template.Models/Organization.cs
+++ Template.Models/Organization.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Template.Models
 {
     public class Organization : IModel, IOrganizationBase
     {
         public string Id { get; set; }
-        [Microsoft.Build.Framework.Required]
+        [Required]
         public string Name { get; set; }
         [Display(Name = "Address")]
-        [Microsoft.Build.Framework.Required]
+        [Required]
         public string StreetAddress1 { get; set; }
         [Display(Name = " ")]
         public string StreetAddress2 { get; set; }
@@ -32,7 +27,7 @@ namespace Template.Models
         [Timestamp]
         public byte[] RowVersion { get; set; }
 
-        public ICollection<Member> Employees { get; set; }
+        public ICollection<Member> Members { get; set; }
     }
 
     
